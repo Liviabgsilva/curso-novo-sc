791f4e445b77a4d1e64f2636bc813dcd16cfa2b8
Mariangel <livia_bg_silva@estudante.sesisenai.org.br>
2026-07-05 11:59:49 -0300

    novo

---
+++ segundomudulo/aula6/module_exemplo/app.js
@@ -0,0 +1,5 @@
+var pessoa = require("./module_exemplo/commons/pessoa");
+
+livia = pessoa();
+
+console.log(JSON.stringify(livia));
---
+++ segundomudulo/aula6/module_exemplo/commons/pessoa.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+    return {
+        nome: "livia",
+        profissao: "Programador",
+        idade: "17",
+        estado: "SC"
+    };
+};
